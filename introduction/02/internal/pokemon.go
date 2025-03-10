8150ca756c9f56463c9180246e2a9984a734e705
Rubén <ruben.hermosilla@capitalexpress.cl>
2025-03-10 14:12:03 -0300

    implement pokemon-cli command with CSV export functionality and PokeAPI integration

--- introduction/02/internal/pokemon.go
+++ introduction/02/internal/pokemon.go
@@ -1,13 +1,14 @@
 package pokemoncli
 
 type Pokemon struct {
-	Id   int
-	Name string
-	Img  string
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Img  string `json:"url"`
 }
 
 type PokemonRepo interface {
 	GetPokemons() ([]Pokemon, error)
+	DumpPokemonsToCSV(pokemons []Pokemon, fileName string)
 }
 
 func NewPokemon(id int, name, img string) Pokemon {
--- introduction/02/internal/storage/pokeapi/repository.go
+++ introduction/02/internal/storage/pokeapi/repository.go
@@ -1,17 +1,30 @@
 package pokeapi
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 	pokemoncli "pokemon-cli/internal"
+	"strconv"
 )
 
 const (
 	pokemonsEndpoint = "/pokemon"
 	pokeapiURL       = "https://pokeapi.co/api/v2"
 )
+
+var apiResponse struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous any    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
 
 type pokemonRepo struct {
 	url string
@@ -29,14 +42,57 @@ func (b *pokemonRepo) GetPokemons() (pokemons []pokemoncli.Pokemon, err error) {
 		return nil, err
 	}
 
-	contents, err := ioutil.ReadAll(response.Body)
+	contents, err := io.ReadAll(response.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	err = json.Unmarshal(contents, &pokemons)
+	err = json.Unmarshal(contents, &apiResponse)
 	if err != nil {
 		return nil, err
 	}
+
+	pokemons = make([]pokemoncli.Pokemon, len(apiResponse.Results))
+	for i, result := range apiResponse.Results {
+		pokemons[i] = pokemoncli.Pokemon{
+			Name: result.Name,
+			Img:  result.URL,
+			Id:   i,
+		}
+	}
 	return
+}
+
+func (b *pokemonRepo) DumpPokemonsToCSV(pokemons []pokemoncli.Pokemon, fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating CSV file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write header
+	header := []string{"ID", "Name", "URL"}
+	if err := writer.Write(header); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing CSV header: %v\n", err)
+		return
+	}
+
+	// Write data rows
+	for _, pokemon := range pokemons {
+		record := []string{
+			strconv.Itoa(pokemon.Id),
+			pokemon.Name,
+			pokemon.Img,
+		}
+		if err := writer.Write(record); err != nil {
+			fmt.Fprintf(os.Stderr, "Error writing pokemon to CSV: %v\n", err)
+			return
+		}
+	}
+
+	fmt.Printf("Successfully exported %d pokemons to %s\n", len(pokemons), fileName)
 }
